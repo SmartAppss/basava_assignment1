aec6999bf20690eb4eefc70880142e5e651a66f0
basavarajmrk <basavaraja.m@smartenovations.com>
2023-09-13 15:14:23 +0530

    update

---
+++ myproject05/app05/forms.py
@@ -0,0 +1,10 @@
+# forms.py in your app
+from django import forms
+from .models import UserInput  # If you have a model
+
+
+class UserInputForm(forms.ModelForm):
+    class Meta:
+        model = UserInput  # Use your model if you have one
+        fields = ['input_field']
+          # Specify the fields you want to collect
---
+++ myproject05/app05/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+"""class products(models.Model):
+    product_name = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.product_name"""
+    
+
+class UserInput(models.Model):
+    input_field = models.CharField(max_length=255)   
+    
+
+    def __str__(self) -> str:
+        return self.input_field
+    
+
+    
+        
